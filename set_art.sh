f224c100be3881ed31c1509805e4fa6b51eaa16d
Federico Bitondo <s276294@studenti.polito.it>
2020-12-31 18:00:59 +0100

    Script for setting a new album art

---
+++ set_art.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# drag and drop the image in the box
+# perform checks on the input URL
+# download and set new album cover
+
+# dependencies:
+# dragon-drag-and-drop
+# wget
+# ffmpeg
+# notify-send
+
+function validate {
+  url=$1
+  egrep "\.(png|jpeg|gif|jpg)\s*$" <<< $url >/dev/null
+}
+
+url=$(dragon-drag-and-drop --target --and-exit)
+validate $url || exit 1
+
+prefix=\$HOME/.config/cmus/arts
+filename=$(basename $url)
+wget --directory-prefix=$prefix $url
+
+file=$(cmus-remote --query | sed -n 's/^file //p')
+temp="${file%.*}.temp.${file##*.}"
+
+ffmpeg -i "$file" -i "$prefix/$filename" -y -map 0 -map 1 \
+  -codec copy -disposition:v:0 attached_pic -map_metadata 0 "$temp" \
+  && mv -f "$temp" "$file" \
+  && notify-send "Set album art" \
+  && cmus_image.sh
+
+exit 0
