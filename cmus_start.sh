e763b2bfcfe926ffb2f50c8e4e5a57e5a11ec6b9
Federico Bitondo <s276294@studenti.polito.it>
2021-02-18 19:19:35 +0100

    Removed mpv from start script

--- cmus_start.sh
+++ cmus_start.sh
@@ -4,10 +4,11 @@
 # if there are no running instance of it, the polybar bar is shown and cmus starts
 # otherwise, the media player status is toggled
 
-if pgrep -x mpv ; then
-  socket=$(find /tmp -maxdepth 1 -type s | head -n1)
-  echo 'cycle pause' | socat - $socket
-elif ! pgrep -x cmus ; then
+#if pgrep -x mpv ; then
+#  socket=$(find /tmp -maxdepth 1 -type s | head -n1)
+#  echo 'cycle pause' | socat - $socket
+#elif ! pgrep -x cmus ; then
+if ! pgrep -x cmus ; then
 	polybar cmusBar -r &
 	alacritty --class cmus -e cmus
 else
